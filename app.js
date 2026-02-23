13691253881779d47c4ebf7e60c6032bf3da04c7
A01662743 <a01662743@tec.mx>
2026-02-22 21:22:28 -0600

    lab 8

--- app.js
+++ app.js
@@ -1,15 +1,68 @@
-console.log("Hola desde node!");
+// Ejercicio 1
+function promedios(arreglo) {
+  let sum = 0;
+  for (num of arreglo) {
+    sum += num;
+  }
+  let promedio = sum / arreglo.length;
+  return promedio;
+}
 
-const filesystem = require("fs");
+console.log(promedios([5, 7, 10]));
 
-filesystem.writeFileSync('hola.txt', 'Hola desde node');
+// Ejercicio 2
+const fs = require("fs");
 
-const http = require('http');
-const server =http.createServer( (request, response) => {
-    console.log(request.url);
-    response.setHeader('Content-Type', 'text/html');
-    response.write(html);
-    response.end();
-})
+function escribirArchivo(texto) {
+  fs.writeFile("file.txt", texto, (error) => {
+    if (error) {
+      console.error("Error al escribir el archivo:", error);
+      return;
+    }
+    console.log("Archivo escrito correctamente.");
+  });
+}
 
-server.listen(3000);
+escribirArchivo("Esta es una prueba, espero que funcione");
+
+// Ejercicio 3 (Fibonacci num)
+function obtenerFibonacci(n) {
+    if (n <= 0) return 0;
+    if (n === 1) return 1;
+
+    let a = 0; // Primer número
+    let b = 1; // Segundo número
+    let temporal;
+
+    for (let i = 2; i <= n; i++) {
+        temporal = a + b;
+        a = b;
+        b = temporal;
+    }
+
+    return b;
+}
+
+//uso
+const posicion = 4;
+console.log(`El número Fibonacci en la posición ${posicion} es: ${obtenerFibonacci(posicion)}`);
+
+
+//despliegue de página html con express
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Servir archivos estáticos (CSS, JS, imágenes) desde la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal: enviar HTML a localhost:3000
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'my first html.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
